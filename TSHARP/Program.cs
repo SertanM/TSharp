91ec0098952b663e5708aa522bd24eb122886e8f
Şerron Karamazov <122390437+SertanM@users.noreply.github.com>
2025-02-01 20:05:34 +0300

    I STARTED!!

---
+++ TSHARP/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Ts
+{
+    sealed class Program 
+    {
+        static void Main(string[] args) 
+        {
+            while (true) 
+            {
+                Console.Write("> ");
+                var line = Console.ReadLine();
+
+                if(string.IsNullOrEmpty(line)) return;
+
+
+            }
+        }
+    }
+
+    public class SyntaxToken
+    {
+        public SyntaxToken(int position, string text)
+        {
+            
+        }
+    }
+
+    public sealed class Lexer
+    {
+        private readonly string _text;
+        private int _position;
+
+        public Lexer(string text)
+        {
+            _text = text;
+        }
+
+        //public SyntaxToken NextToken()
+        //{
+
+        //}
+    }
+}
